f2644f129d0c61d271f5dff55c68b110446d075e
Howon Lee <howon@howonlee.com>
2015-09-23 11:06:44 -0700

    skeleton of some tests for constructor

--- constructorio/constructorio.py
+++ constructorio/constructorio.py
@@ -1,11 +1,34 @@
 import requests
 
 class Constructor:
-    def __init__(self, stuff):
+    VERSION = "1.0.0"
+
+    def __init__(self, api_key, autocomplete_key, url="ac.cnstrc.com"):
+        self.api_key = api_key
+        self.ac_key = ac_key
+        self.url = url
+
+    def __version__(self):
+        return VERSION
+
+    def autocomplete(self, query):
+        requests.post(self.url, something, something)
         pass
 
-    def autocomplete_stuff(self):
+    def add(self, item_name, autocomplete_section):
         pass
 
-    def other_stuff(self):
+    def remove(self):
+        pass
+
+    def modify(self):
+        pass
+
+    def track_conversion(self):
+        pass
+
+    def track_click_through(self):
+        pass
+
+    def track_search(self):
         pass
--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,1 +1,42 @@
 import pytest
+import constructorio
+
+class something:
+    def test_encodes_parameters():
+        var constructorio = new Constructorio({})
+        assert.equal(constructorio.client._serializeParams({foo: [1, 2], bar: {baz: ['a', 'b']}}), 'foo%5B%5D=1&foo%5B%5D=2&bar%5Bbaz%5D%5B%5D=a&bar%5Bbaz%5D%5B%5D=b')
+
+    def test_creates_urls_correctly():
+        var constructorio = new Constructorio({
+          autocompleteKey: 'a-test-autocomplete-key',
+        })
+        assert.equal(constructorio.client._makeUrl('test'), 'https://ac.cnstrc.com/v1/test?autocomplete_key=a-test-autocomplete-key')
+
+    def test_set_api_token():
+        var apiToken = 'a-test-api-key',
+        constructorio = new Constructorio({ apiToken: apiToken })
+
+        assert.equal(constructorio.config.apiToken, apiToken)
+
+    def test_set_ac_key():
+        var autocompleteKey = 'a-test-autocomplete-key',
+            constructorio = new Constructorio({ autocompleteKey: autocompleteKey })
+        assert.equal(constructorio.config.autocompleteKey, autocompleteKey)
+
+    def test_add():
+        pass
+
+    def test_remove():
+        pass
+
+    def test_modify():
+        pass
+
+    def test_conversion():
+        pass
+
+    def test_search():
+        pass
+
+    def test_click_through():
+        pass
