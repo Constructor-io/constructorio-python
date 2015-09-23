1e709004d72158b126e7ffa2fbfdac27fa44206d
Howon Lee <howon@howonlee.com>
2015-09-23 11:25:25 -0700

    work on tests

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,27 +1,27 @@
 import pytest
-import constructorio
+from constructorio import ConstructorIO
 
 class something:
     def test_encodes_parameters():
-        var constructorio = new Constructorio({})
-        assert.equal(constructorio.client._serializeParams({foo: [1, 2], bar: {baz: ['a', 'b']}}), 'foo%5B%5D=1&foo%5B%5D=2&bar%5Bbaz%5D%5B%5D=a&bar%5Bbaz%5D%5B%5D=b')
+        constructor = ConstructorIO()
+        serialized_params = constructor\
+            ._serializeParams({foo: [1, 2], bar: {baz: ['a', 'b']}})
+        assert serialized_params == 'foo%5B%5D=1&foo%5B%5D=2&bar%5Bbaz%5D%5B%5D=a&bar%5Bbaz%5D%5B%5D=b'
 
     def test_creates_urls_correctly():
-        var constructorio = new Constructorio({
-          autocompleteKey: 'a-test-autocomplete-key',
-        })
-        assert.equal(constructorio.client._makeUrl('test'), 'https://ac.cnstrc.com/v1/test?autocomplete_key=a-test-autocomplete-key')
+        constructor = ConstructorIO(autocompleteKey="a-test-autocomplete-key")
+        generated_url = constructor._makeUrl('test')
+        assert generated_url == 'https://ac.cnstrc.com/v1/test?autocomplete_key=a-test-autocomplete-key'
 
     def test_set_api_token():
-        var apiToken = 'a-test-api-key',
-        constructorio = new Constructorio({ apiToken: apiToken })
-
-        assert.equal(constructorio.config.apiToken, apiToken)
+        apiToken = 'a-test-api-key',
+        constructor = ConstructorIO(apiToken=apiToken)
+        assert constructor._apiToken == apiToken
 
     def test_set_ac_key():
-        var autocompleteKey = 'a-test-autocomplete-key',
-            constructorio = new Constructorio({ autocompleteKey: autocompleteKey })
-        assert.equal(constructorio.config.autocompleteKey, autocompleteKey)
+        autocompleteKey = 'a-test-autocomplete-key'
+        constructor = CosntructorIO(autocompleteKey=autocompleteKey)
+        assert constructor._autocompleteKey == autocompleteKey
 
     def test_add():
         pass
