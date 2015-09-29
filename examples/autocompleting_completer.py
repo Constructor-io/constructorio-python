5714d6355ee144d11688e5035b8715ddfa44b51b
Howon Lee <howon@howonlee.com>
2015-09-29 16:49:01 -0700

    spiffier cli for completer

--- examples/autocompleting_completer.py
+++ examples/autocompleting_completer.py
@@ -4,19 +4,7 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 # hack for easy import of the library
 
-import readline
 from constructorio import ConstructorIO
-
-class Completer(object):
-    def __init__(self, autocompleteKey):
-        self.constructor = ConstructorIO("invalid", autocompleteKey)
-
-    def complete(self, text, state):
-        query = self.constructor.query(text)
-        if len(query["suggestions"]) == 0:
-            state -= 1
-        else:
-            return query["suggestions"][0]["value"]
 
 if __name__ == "__main__":
     # Standard English Autocomplete
@@ -26,4 +14,6 @@ if __name__ == "__main__":
         if misspelled == "q":
             break
         else:
-            print constructor.query(misspelled)
+            query = constructor.query(misspelled)
+            suggested_queries = [s["value"] for s in query["suggestions"]]
+            print "suggested queries: ", " | ".join(suggested_queries)
