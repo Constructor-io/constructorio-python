28bcc119052c2f092ca6c59e14ee5b5cba63b4f9
Eli Finkelshteyn <iefinkel@gmail.com>
2016-01-01 19:25:47 -0800

    bumping to version 0.0.3

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
         name="constructor-io",
-        version="0.0.2",
+        version="0.0.3",
         license="MIT",
         description="Constructor.IO Python Client",
-        author="Howon Lee",
-        author_email="howon@constructor.io",
+        author="Constructor.io",
+        author_email="info@constructor.io",
         url="https://www.constructor.io",
         install_requires=[
             "requests",
