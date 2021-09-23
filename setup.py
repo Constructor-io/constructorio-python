5a8772388a0192020385b91651df09d8f0d25578
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2021-09-23 10:43:34 -0400

    Add download_url to setup

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ VERSION = "0.0.12"
 setup(
     name="constructorio_python",
     version=VERSION,
+    download_url='https://github.com/Constructor-io/constructorio-python/tarball/' + VERSION,
     license="MIT",
     description="Constructor.IO Python Client",
     author="Constructor.io",
