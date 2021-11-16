7b85ac4bc1bd06ad2310e4554c59da4e11d1c26a
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2021-11-16 09:53:42 -0500

    Exclude tests and add description

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 from constructor_io import __version__
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="constructor-io",
@@ -14,5 +19,7 @@ setup(
     install_requires=[
         'requests~=2.26'
     ],
-    packages=find_packages(exclude=("tests",)),
+    packages = find_packages(exclude=["tests.*", "tests"]),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
