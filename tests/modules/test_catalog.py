f549bffb987c89e79bb6c0757595bff7bff8e53f
Stanley Peng <speng1011@gmail.com>
2022-03-15 10:22:12 -0700

    Fix test

--- tests/modules/test_catalog.py
+++ tests/modules/test_catalog.py
@@ -95,7 +95,7 @@ def test_with_replace_no_files():
 
     with raises(
         HttpException,
-        match=r'form-data should contain at least one of "items", "variations", "item_groups" file(s)' #pylint: disable=line-too-long
+        match=r'form-data should contain at least one of "items", "variations", "item_groups'
     ):
         catalog.replace_catalog(data)
 
@@ -167,7 +167,7 @@ def test_with_update_no_files():
 
     with raises(
         HttpException,
-        match=r'form-data should contain at least one of "items", "variations", "item_groups" file(s)' #pylint: disable=line-too-long
+        match=r'form-data should contain at least one of "items", "variations", "item_groups'
     ):
         catalog.update_catalog(data)
 
@@ -239,7 +239,7 @@ def test_with_patch_no_files():
 
     with raises(
         HttpException,
-        match=r'form-data should contain at least one of "items", "variations", "item_groups" file(s)' #pylint: disable=line-too-long
+        match=r'form-data should contain at least one of "items", "variations", "item_groups'
     ):
         catalog.patch_catalog(data)
 
